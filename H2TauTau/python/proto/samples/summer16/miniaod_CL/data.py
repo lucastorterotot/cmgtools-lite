f25bd45d2542222b13f8bb777a3ea6b8bc103088
lucastorterotot <lucas.torterotot@gmail.com>
2018-06-29 13:55:57 +0200

    Data sample correction - samlpe prepared as MC instead of data

--- H2TauTau/python/proto/samples/summer16/miniaod_CL/data.py
+++ H2TauTau/python/proto/samples/summer16/miniaod_CL/data.py
@@ -5,10 +5,14 @@ from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
 creator = ComponentCreator()
 dbsInstance='phys03'
 
-data_single_muon_B_to_G = creator.makeMCComponent(
+json='/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+
+#kreator.makeDataComponent("JetHT_Run2016B_PromptReco_v2"         , "/JetHT/Run2016B-PromptReco-v2/MINIAOD"         , "CMS",      ".*root", json)
+
+data_single_muon_B_to_G = creator.makeDataComponent(
     "data_single_muon", 
-   "/SingleMuon/cbernet-MINIAOD_CL_2-3ac6ceef15bb149c4b22dc59e3cfeade/USER",
-    "CMS", ".*root", 1.0, dbsInstance=dbsInstance)
+   "/SingleMuon/cbernet-MINIAOD_CL_2-3ac6ceef15bb149c4b22dc59e3cfeade/USER", # not the right one ?
+    "CMS", ".*root", json)
 
 data_single_muon = [data_single_muon_B_to_G]
 
