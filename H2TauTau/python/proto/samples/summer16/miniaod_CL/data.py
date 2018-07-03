e756957e458061a42a6908600e711bf58e0f5ee0
lucastorterotot <lucas.torterotot@gmail.com>
2018-07-03 17:54:42 +0200

    JSON file now available with CRAB

--- H2TauTau/python/proto/samples/summer16/miniaod_CL/data.py
+++ H2TauTau/python/proto/samples/summer16/miniaod_CL/data.py
@@ -1,11 +1,12 @@
 import copy
+import os
 
 from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
 
 creator = ComponentCreator()
 dbsInstance='phys03'
 
-json='/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+json= os.path.expandvars('$CMSSW_BASE/src/CMGTools/H2TauTau/data/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt') # check ?
 
 #kreator.makeDataComponent("JetHT_Run2016B_PromptReco_v2"         , "/JetHT/Run2016B-PromptReco-v2/MINIAOD"         , "CMS",      ".*root", json)
 
