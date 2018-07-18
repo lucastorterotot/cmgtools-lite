c00f14f6f1a67307b40786ea21cdad5619d36888
Colin Bernet <colin.bernet@cern.ch>
2018-07-18 17:02:23 +0200

    started modularizing

---
+++ H2TauTau/python/heppy/analyzers/MuAnalyzer.py
@@ -0,0 +1,22 @@
+from PhysicsTools.Heppy.analyzers.core.Analyzer import Analyzer
+from PhysicsTools.Heppy.analyzers.core.AutoHandle import AutoHandle
+from PhysicsTools.Heppy.physicsobjects.Muon import Muon
+
+class MuAnalyzer(DiLeptonAnalyzer):
+
+    def declareHandles(self):
+        super(MuAnalyzer, self).declareHandles()
+
+        self.handles['muons'] = AutoHandle(
+            self.cfg_ana.muons,
+            'std::vector<pat::Muon>'
+        )
+        
+    def process(self, event):
+        muons = self.handles['muons'].product()
+        muons = map(Muon, muons)
+        setattr(event, self.cfg_ana.output, muons)
+        
+    def evaluate_tauid(self, muons):
+        pass
+    
---
+++ H2TauTau/python/heppy/analyzers/TauAnalyzer.py
@@ -0,0 +1,26 @@
+from PhysicsTools.Heppy.analyzers.core.Analyzer import Analyzer
+from PhysicsTools.Heppy.analyzers.core.AutoHandle import AutoHandle
+from PhysicsTools.Heppy.physicsobjects.Tau import Tau
+
+class TauAnalyzer(DiLeptonAnalyzer):
+
+    def declareHandles(self):
+        super(TauAnalyzer, self).declareHandles()
+
+        self.handles['taus'] = AutoHandle(
+            self.cfg_ana.taus,
+            'std::vector<pat::Tau>'
+        )
+        
+    def process(self, event):
+        taus = self.handles['taus'].product()
+        taus = map(Tau, taus)
+        self.evaluate_tauid(taus)
+        # self.correct_energy(taus)
+        setattr(event, self.cfg_ana.output, taus)
+        
+    def evaluate_tauid(self, taus):
+        pass
+    
+    def correct_energy(self, taus):
+        pass
