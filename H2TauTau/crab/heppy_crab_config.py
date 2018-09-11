74183c0e9c2a17f7a3d70cd4bb8292f1df75f818
lucastorterotot <lucas.torterotot@gmail.com>
2018-09-11 13:46:14 +0200

    moved files from cfgpython/crab/ to crab/

---
+++ H2TauTau/crab/heppy_crab_config.py
@@ -0,0 +1,69 @@
+import os, imp, datetime
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'heppy_' + os.environ["DATASET"] + "_" + os.environ["CMSSW_VERSION"] +  "_" + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') # task name
+config.General.workArea = 'heppy_crab_projects'
+
+config.section_("General")
+config.General.transferLogs = True
+
+config.section_("JobType")
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = os.environ["CMSSW_BASE"]+'/src/CMGTools/H2TauTau/crab/heppy_crab_fake_pset.py'
+config.JobType.scriptExe = os.environ["CMSSW_BASE"]+'/src/CMGTools/H2TauTau/crab/heppy_crab_script.sh'
+#config.JobType.disableAutomaticOutputCollection = True
+# config.JobType.sendPythonFolder = True  #doesn't work, not supported yet? do it by hand
+
+config.JobType.inputFiles = [
+    os.environ["CMSSW_BASE"]+'/src/CMGTools/H2TauTau/crab/FrameworkJobReport.xml',
+    os.environ["CMSSW_BASE"]+'/src/CMGTools/H2TauTau/crab/heppy_crab_script.py',
+    'cmgdataset.tar.gz',
+    'python.tar.gz',
+    'cafpython.tar.gz',
+    'options.json'
+]
+config.JobType.inputFiles.append(os.environ["CFG_FILE"])
+
+config.JobType.outputFiles = []
+
+config.section_("Data")
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 10
+config.Data.totalUnits = config.Data.unitsPerJob * int(os.environ["NJOBS"])
+
+config.Data.outLFNDirBase  = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.outLFNDirBase += '/heppy_crab/' + os.environ["CMSSW_VERSION"]
+
+config.Data.publication = False
+
+config.section_("Site")
+config.Site.whitelist = [
+    'T3_FR_IPNL',
+    # "T2_CH_CSCS", 
+    # "T2_IT_Legnaro", 
+    # "T2_UK_London_IC", 
+    # "T2_UK_SGrid_Bristol", 
+    # "T2_DE_DESY", 
+    # "T2_ES_CIEMAT", 
+    # "T2_IT_Rome", 
+    # "T2_AT_Vienna",
+    # "T2_DE_RWTH",
+    # "T2_FR_GRIF_IRFU", 
+    # "T2_HU_Budapest", 
+    # "T2_FR_IPHC", 
+    # "T2_BE_IIHE", 
+    # "T2_IT_Pisa", 
+    # "T2_ES_IFCA", 
+    # "T2_UK_London_Brunel", 
+    # "T2_US_Purdue", 
+    # "T2_UA_KIPT", 
+    # "T2_US_MIT", 
+    # "T2_US_Wisconsin", 
+    # "T2_US_UCSD", 
+    # "T2_US_Vanderbilt", 
+    # "T2_US_Caltech",
+]
+
+config.Site.storageSite = 'T3_FR_IPNL'
