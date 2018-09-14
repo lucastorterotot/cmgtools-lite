c89fb50be7aa0a309f9c87955ba02f04e3045b99
Gael <gael.touquet@gmail.com>
2018-09-14 13:30:14 +0200

    update fully hadronic triggers

--- H2TauTau/python/heppy/sequence/common.py
+++ H2TauTau/python/heppy/sequence/common.py
@@ -180,7 +180,7 @@ from CMGTools.H2TauTau.heppy.analyzers.TrigMatcher import TrigMatcher
 trigger_match = cfg.Analyzer(
     TrigMatcher,
     src='dileptons_sorted',
-    require_all_matched = False
+    require_all_matched = True
 )
 
 
