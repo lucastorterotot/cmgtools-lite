e51aac728d8d4043c15decdfba0c7f7d3303302a
Gael <gael.touquet@gmail.com>
2018-09-27 10:14:32 +0200

    b-tagging tuned for sync (old SF file)

--- H2TauTau/python/heppy/analyzers/BJetAnalyzer.py
+++ H2TauTau/python/heppy/analyzers/BJetAnalyzer.py
@@ -16,7 +16,7 @@ class BJetAnalyzer(Analyzer):
             jet.is_btagged = self.btagSF.isBTagged(pt=jet.pt(),
                                                    eta=jet.eta(),
                                                    csv=jet.btag(
-                    "pfDeepCSVDiscriminatorsJetTags:BvsAll"),
+                    "pfDeepCSVJetTagsprobbb+pfDeepCSVJetTagsprobb"),
                                                    jetflavor=abs(jet.hadronFlavour()),
                                                    is_data=not self.cfg_comp.isMC,
                                                    csv_cut=0.4941)
