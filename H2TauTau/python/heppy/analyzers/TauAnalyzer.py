c471af051719bec094121e85dfc130032253403e
lucastorterotot <lucas.torterotot@gmail.com>
2018-10-15 14:17:42 +0200

    Tau scale factor implemented -- tau.gen_match remains to be set before this correction.

--- H2TauTau/python/heppy/analyzers/TauAnalyzer.py
+++ H2TauTau/python/heppy/analyzers/TauAnalyzer.py
@@ -19,9 +19,21 @@ class TauAnalyzer(Analyzer):
         for tau in taus:
             htau = Tau(tau)
             htau.associatedVertex = event.vertices[0]
+            self.attach_tau_gen_match(tau)
+            self.correct_energy(tau)
             htaus.append(htau)
-        # self.correct_energy(taus)
         setattr(event, self.cfg_ana.output, htaus)
  
-    def correct_energy(self, taus):
-        pass
+    def correct_energy(self, tau):
+        if hasattr(tau, 'gen_match') :
+            if tau.gen_match == 5 :
+                if tau.decayMode == 0 : # h
+                    tau.scaleEnergy(0.97)
+                elif tau.decayMode == 1 : # h p0
+                    tau.scaleEnergy(0.98)
+                elif tau.decayMode == 10 : # hhh
+                    tau.scaleEnergy(0.99)
+                # elif tau.decayMode == 11 ? : # hhh p0
+        else:
+            print 'No gen match for tau lepton!'
+            
