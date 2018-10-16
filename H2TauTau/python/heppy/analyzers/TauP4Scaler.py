a313c7a7a7ca3e4c707ce606d8a18a3d7aaaf604
lucastorterotot <lucas.torterotot@gmail.com>
2018-10-16 16:25:16 +0200

    Correction to TauP4Scaler.

--- H2TauTau/python/heppy/analyzers/TauP4Scaler.py
+++ H2TauTau/python/heppy/analyzers/TauP4Scaler.py
@@ -11,11 +11,11 @@ class TauP4Scaler(Analyzer):
     def correct_energy(self, tau):
         if hasattr(tau, 'gen_match') :
             if tau.gen_match == 5 :
-                if tau.decayMode == 0 : # h
+                if tau.decayMode() == 0 : # h
                     tau.scaleEnergy(0.97)
-                elif tau.decayMode == 1 : # h p0
+                elif tau.decayMode() == 1 : # h p0
                     tau.scaleEnergy(0.98)
-                elif tau.decayMode == 10 : # hhh
+                elif tau.decayMode() == 10 : # hhh
                     tau.scaleEnergy(0.99)
                 # elif tau.decayMode == 11 ? : # hhh p0
         else:
