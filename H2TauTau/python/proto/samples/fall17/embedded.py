9f27719f6c25071848956238dd77d217553feb37
lucastorterotot <lucas.torterotot@gmail.com>
2019-04-05 16:20:11 +0200

    Embedding samples updated

--- H2TauTau/python/proto/samples/fall17/embedded.py
+++ H2TauTau/python/proto/samples/fall17/embedded.py
@@ -11,19 +11,19 @@ Embedded2017F_tt = creator.makeDataComponent('Embedded2017F_tt', '/EmbeddingRun2
 embedded_tt = [Embedded2017B_tt,Embedded2017C_tt,Embedded2017D_tt,Embedded2017E_tt,Embedded2017F_tt]
 
 
-Embedded2017B_mt = creator.makeDataComponent('Embedded2017B_mt', '/EmbeddingRun2017B/MuTauFinalState-inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root',dbsInstance='phys03')
-Embedded2017C_mt = creator.makeDataComponent('Embedded2017C_mt', '/EmbeddingRun2017C/MuTauFinalState-inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root',dbsInstance='phys03')
-Embedded2017D_mt = creator.makeDataComponent('Embedded2017D_mt', '/EmbeddingRun2017D/MuTauFinalState-inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root',dbsInstance='phys03')
-Embedded2017E_mt = creator.makeDataComponent('Embedded2017E_mt', '/EmbeddingRun2017E/MuTauFinalState-inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root',dbsInstance='phys03')
-Embedded2017F_mt = creator.makeDataComponent('Embedded2017F_mt', '/EmbeddingRun2017F/MuTauFinalState-inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root',dbsInstance='phys03')
+Embedded2017B_mt = creator.makeDataComponent('Embedded2017B_mt', '/EmbeddingRun2017B/MuTauFinalState-StoreResults_inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root')
+Embedded2017C_mt = creator.makeDataComponent('Embedded2017C_mt', '/EmbeddingRun2017C/MuTauFinalState-StoreResults_inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root')
+Embedded2017D_mt = creator.makeDataComponent('Embedded2017D_mt', '/EmbeddingRun2017D/MuTauFinalState-StoreResults_inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root')
+Embedded2017E_mt = creator.makeDataComponent('Embedded2017E_mt', '/EmbeddingRun2017E/MuTauFinalState-StoreResults_inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root')
+Embedded2017F_mt = creator.makeDataComponent('Embedded2017F_mt', '/EmbeddingRun2017F/MuTauFinalState-StoreResults_inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root')
 
 embedded_mt = [Embedded2017B_mt,Embedded2017C_mt,Embedded2017D_mt,Embedded2017E_mt,Embedded2017F_mt]
 
 
-Embedded2017B_et = creator.makeDataComponent('Embedded2017B_et', '/EmbeddingRun2017B/ElTauFinalState-inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root',dbsInstance='phys03')
-Embedded2017C_et = creator.makeDataComponent('Embedded2017C_et', '/EmbeddingRun2017C/ElTauFinalState-inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root',dbsInstance='phys03')
-Embedded2017D_et = creator.makeDataComponent('Embedded2017D_et', '/EmbeddingRun2017D/ElTauFinalState-inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root',dbsInstance='phys03')
-Embedded2017E_et = creator.makeDataComponent('Embedded2017E_et', '/EmbeddingRun2017E/ElTauFinalState-inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root',dbsInstance='phys03')
-Embedded2017F_et = creator.makeDataComponent('Embedded2017F_et', '/EmbeddingRun2017F/ElTauFinalState-inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root',dbsInstance='phys03')
+Embedded2017B_et = creator.makeDataComponent('Embedded2017B_et', '/EmbeddingRun2017B/ElTauFinalState-StoreResults_inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root')
+Embedded2017C_et = creator.makeDataComponent('Embedded2017C_et', '/EmbeddingRun2017C/ElTauFinalState-StoreResults_inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root')
+Embedded2017D_et = creator.makeDataComponent('Embedded2017D_et', '/EmbeddingRun2017D/ElTauFinalState-StoreResults_inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root')
+Embedded2017E_et = creator.makeDataComponent('Embedded2017E_et', '/EmbeddingRun2017E/ElTauFinalState-StoreResults_inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root')
+Embedded2017F_et = creator.makeDataComponent('Embedded2017F_et', '/EmbeddingRun2017F/ElTauFinalState-StoreResults_inputDoubleMu_94X_miniAOD-v2/USER', 'CMS', '.*root')
 
 embedded_et = [Embedded2017B_et,Embedded2017C_et,Embedded2017D_et,Embedded2017E_et,Embedded2017F_et]
