a8585e0bfaafaa9071d2c423966e506c8573816b
lucastorterotot <lucas.torterotot@gmail.com>
2019-04-09 14:33:58 +0200

    Apply FF option added, harvest cleaning on duplicated code

--- H2TauTau/scripts/multiHarvest.py
+++ H2TauTau/scripts/multiHarvest.py
@@ -36,6 +36,9 @@ def get_options():
     parser.add_option("-c", "--cores", dest = "ncores",
                       default=20,
                       help='Number of cores on which to parralelise harvesting')
+    parser.add_option("-F", "--ff", dest="apply_ff",
+                      default=True,
+                      help='whether or not to add fake factors to trees')
     
     (options,args) = parser.parse_args()
     return options, args
@@ -106,7 +109,7 @@ while start_harvest not in ['y','n']:
     start_harvest = raw_input('Harvest this list? [y/n]')
 if start_harvest == 'y':
     print 'Starting to harvest.'
-    # multithreadmap(harvest, to_harvest, ncores=1+0*options.ncores, subdir_pattern=optionssubdir_pattern, tgz_pattern=options.tgz_pattern, skim=options.skim)
+    # multithreadmap(harvest, to_harvest, ncores=1+0*options.ncores, subdir_pattern=optionssubdir_pattern, tgz_pattern=options.tgz_pattern, skim=options.skim, apply_ff=options.apply_ff)
     # harvest(to_harvest[0])
 else:
     print 'Aborting.'
