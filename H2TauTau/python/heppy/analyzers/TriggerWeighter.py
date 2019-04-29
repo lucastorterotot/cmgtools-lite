338155cf231c9e7c643e9c63d2840967ebfc9282
lucastorterotot <lucas.torterotot@gmail.com>
2019-04-29 16:49:26 +0200

    TriggerWeighter docstring.

--- H2TauTau/python/heppy/analyzers/TriggerWeighter.py
+++ H2TauTau/python/heppy/analyzers/TriggerWeighter.py
@@ -3,6 +3,35 @@ from ROOT import TFile
 
 class TriggerWeighter(Analyzer):
     '''Adds the trigger weight(s) to the given legs of the event.
+
+    Example:
+    ws_tau_vars_dict = {'t_pt':lambda tau:tau.pt(),
+                        't_eta':lambda tau:tau.eta(),
+                        't_phi':lambda tau:tau.phi()}
+    ws_tau_func_dict = {'tt':'t_trg_tight_tt_ratio'}
+    from CMGTools.H2TauTau.heppy.analyzers.TriggerWeighter import TriggerWeighter
+    triggerweighter = cfg.Analyzer(
+        TriggerWeighter,
+        'TriggerWeighter',
+        workspace_path = '$CMSSW_BASE/src/CMGTools/H2TauTau/data/htt_scalefactors_2017_v2.root',
+        legs = lambda event: [event.dileptons_sorted[0].leg1(),event.dileptons_sorted[0].leg2()],
+        leg1_vars_dict = ws_tau_vars_dict,
+        leg2_vars_dict = ws_tau_vars_dict,
+        leg1_func_dict = ws_tau_func_dict,
+        leg2_func_dict = ws_tau_func_dict
+    )
+
+    @param legs: the legs of the event
+
+    @param workspace_path: the rootfile containing the workspace
+
+    @param legX_vars_dict: the variables used in the workspace function
+
+    @param legX_func_dict: the function for each trigger type to be used to
+    compute the weight
+
+    the trigger types matched by the leg are important as one weight is applied
+    for each trigger type.
     '''
     
     def __init__(self, cfg_ana, cfg_comp, looperName):
