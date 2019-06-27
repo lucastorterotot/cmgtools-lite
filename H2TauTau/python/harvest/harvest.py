220344466ca35ae28d730f9da45b15767e026e9f
Colin Bernet <colin.bernet@cern.ch>
2019-06-27 17:30:36 +0200

    multiprocessing working: eliminated Harvester class

--- H2TauTau/python/harvest/harvest.py
+++ H2TauTau/python/harvest/harvest.py
@@ -3,8 +3,11 @@ import shutil
 import tempfile 
 import time
 
-from datasetdb import DatasetDB
 from dataset import Dataset
+from multiprocessing import Pool
+
+# to be set by user
+datasetdb = None
 
 def get_options():
      from optparse import OptionParser
@@ -22,125 +25,135 @@ def get_options():
           sys.exit(1)
      return options, args
      
+def wait_a_bit(nsec):
+     time.sleep(nsec)
+     print('done')
 
-class Harvester(object): 
-     '''Harvests datasets
-     Holds a connection to the database. 
-     Manages harvesting for a collection of datasets
-     '''
 
-     def __init__(self, dataset_db):
-          '''initialize the harvester with a connection to the db.
-          dataset_db is of type datasetdb.DatasetDB
-          '''
-          self.dsdb = dataset_db
-
+def harvest_one(info, destination, ntgzs=None):
+     '''harvest one dataset
      
-     def get_ds_infos(self, coll, regex): 
-          '''returns infos for datasets with a name matching regex
-          in collection coll
-          '''
-          return self.dsdb.find_by_name(coll, regex)
+     dataset is: 
+     - fetched
+     - unpacked
+     - hadded 
+     - copied to destination on lyovis10 (don't forget the tunnel)
+     
+     Finally, its info is added to the harv table in the db
+     '''
+     print('there')
+     tmpdir = tempfile.mkdtemp()
+     fetch(info, tmpdir, ntgzs)
+     unpack(info, tmpdir, ntgzs)
+     hadd(tmpdir)
+     sampledir = '/'.join([tmpdir,info['name']])
+     # print(sampledir)
+     scp(sampledir, 'localhost:'+destination, '-P 2222')
+     shutil.rmtree(tmpdir)
+     del info['_id']
+     info['harv_time'] = time.time()
+     info['harv_dir'] = sampledir
+     return info 
 
-     def harvest(self, infos, destination): 
-          '''harvest the datasets corresponding to infos
-          the data is stored in the destination directory 
-          IMPLEMENT MULTIPROCESSING MODE
-          '''
+def get_ds_infos(coll, regex): 
+     '''returns infos for datasets with a name matching regex
+     in collection coll
+     '''
+     return datasetdb.find_by_name(coll, regex)
+
+
+def harvest(infos, destination, nworkers=None, ntgzs=None): 
+     '''harvest the datasets corresponding to infos
+     the data is stored in the destination directory 
+     IMPLEMENT MULTIPROCESSING MODE
+     '''
+     hinfos = []
+     if nworkers is None or nworkers==1:
           for info in infos: 
-               self.harvest_one(info, destination)
-
-     def harvest_one(self, info, destination, ntgzs=None):
-          '''harvest one dataset
-          
-          dataset is: 
-          - fetched
-          - unpacked
-          - hadded 
-          - copied to destination on lyovis10 (don't forget the tunnel)
-          
-          Finally, its info is added to the harv table in the db
-          '''
-          tmpdir = tempfile.mkdtemp()
-          self.fetch(info, tmpdir, ntgzs)
-          self.unpack(info, tmpdir, ntgzs)
-          self.hadd(tmpdir)
-          sampledir = '/'.join([tmpdir,info['name']])
-          # print(sampledir)
-          self.scp(sampledir, 'localhost:'+destination, '-P 2222')
-          shutil.rmtree(tmpdir)
-          del info['_id']
-          info['harv_time'] = time.time()
-          info['harv_dir'] = sampledir
-          self.dsdb.insert('harvested', info)
-
-     def fetch(self, info, destination, ntgzs=None):
-          '''fetch the dataset and put it into a destination directory
-          ntgzs : maximum number of tgzs to fetch. used for testing
-          '''
-          ds = Dataset(info)
-          basepath=os.getcwd()
-          os.chdir(destination)
-          destpath=os.getcwd()
-          for subd, files in ds.info()['tgzs'].iteritems():
-               print 'fetching subdir', subd
-               os.mkdir(subd)
-               os.chdir(subd)
-               if ntgzs is None: 
-                    ntgzs = len(files)
-               for f in files[:ntgzs]:
-                    path = ds.abspath('/'.join([subd, f]))
-                    print path
-                # print self.fhandler.ls(path)
-                    ds.fhandler.cp(path, '.')
-               os.chdir(destpath)
-          os.chdir(basepath)
-          return True
+               hinfos.append(harvest_one(info, destination, ntgzs))
+     else: 
+          p = Pool(nworkers)
+          futures = []
+          for info in infos: 
+               futures.append( p.apply_async( harvest_one, 
+                                              (info, destination, ntgzs)) )
+          for future in futures: 
+               hinfos.append(future.get())
+          print('here')
+          p.close()
+          p.join()
+     for hinfo in hinfos: 
+          datasetdb.insert('harvested', hinfo)
 
 
-     def scp(self, directory, destination, options=''): 
-          '''scp the directory to a destination'''
-          cmd = 'scp -r {} {} {}'.format(
-               options, directory, destination
-               )
-          os.system(cmd)
+def fetch(info, destination, ntgzs=None):
+     '''fetch the dataset and put it into a destination directory
+     ntgzs : maximum number of tgzs to fetch. used for testing
+     '''
+     ds = Dataset(info)
+     basepath=os.getcwd()
+     os.chdir(destination)
+     destpath=os.getcwd()
+     for subd, files in ds.info()['tgzs'].iteritems():
+          print 'fetching subdir', subd
+          os.mkdir(subd)
+          os.chdir(subd)
+          if ntgzs is None: 
+               ntgzs = len(files)
+          for f in files[:ntgzs]:
+               path = ds.abspath('/'.join([subd, f]))
+               print path
+               ds.fhandler.cp(path, '.')
+          os.chdir(destpath)
+     os.chdir(basepath)
+     return True
 
-     def hadd(self, directory):
-          '''Run heppy_hadd in directory, 
-          and remove the chunks'''
-          oldpath=os.getcwd()
-          os.chdir(directory)
-          os.system('heppy_hadd.py .')
-          os.system('rm -rf *Chunk*')
-          os.chdir(oldpath)
 
-     def unpack(self, info, destination, ntgzs=None):
-          '''unpacks the dataset after fetching. 
-          each tgz file is unpacked into an heppy chunk, 
-          for later heppy_hadd
-          '''
-          oldpath=os.getcwd()
-          os.chdir(destination)
-          destpath = os.getcwd()
-          chunks = dict()
-          for subd, tgzs in info['tgzs'].iteritems():
-               # print 'unpacking subdir', subd
-               subdid = int(subd)
-               os.chdir(subd)
-               chunks[subd] = []
-               if ntgzs is None:
-                    ntgzs = len(tgzs)
-               for tgz in tgzs[:ntgzs]: 
-                    print 'unpacking', tgz
-                    os.system('tar -zxf {}'.format(tgz))
-                    # tgz is e.g. heppyOutput_43.tgz
-                    # so index is 43
-                    index = int(os.path.splitext(tgz)[0].split('_')[1]) + subdid
-                    chunkname = '{}_Chunk{}'.format(info['name'], index)
-                    chunks[subd].append(chunkname)
-                    os.rename('Output', '../'+chunkname)
-                    os.remove(tgz)
-               os.chdir(destpath)
-               shutil.rmtree(subd)
-          os.chdir(oldpath)
+def scp(directory, destination, options=''): 
+     '''scp the directory to a destination'''
+     cmd = 'scp -r {} {} {}'.format(
+          options, directory, destination
+          )
+     os.system(cmd)
+
+
+def hadd(directory):
+     '''Run heppy_hadd in directory, 
+     and remove the chunks'''
+     oldpath=os.getcwd()
+     os.chdir(directory)
+     os.system('heppy_hadd.py .')
+     os.system('rm -rf *Chunk*')
+     os.chdir(oldpath)
+
+
+def unpack(info, destination, ntgzs=None):
+     '''unpacks the dataset after fetching. 
+     each tgz file is unpacked into an heppy chunk, 
+     for later heppy_hadd
+     '''
+     oldpath=os.getcwd()
+     os.chdir(destination)
+     destpath = os.getcwd()
+     chunks = dict()
+     for subd, tgzs in info['tgzs'].iteritems():
+          # print 'unpacking subdir', subd
+          subdid = int(subd)
+          os.chdir(subd)
+          chunks[subd] = []
+          if ntgzs is None:
+               ntgzs = len(tgzs)
+          for tgz in tgzs[:ntgzs]: 
+               print 'unpacking', tgz
+               os.system('tar -zxf {}'.format(tgz))
+               # tgz is e.g. heppyOutput_43.tgz
+               # so index is 43
+               index = int(os.path.splitext(tgz)[0].split('_')[1]) + subdid
+               chunkname = '{}_Chunk{}'.format(info['name'], index)
+               chunks[subd].append(chunkname)
+               os.rename('Output', '../'+chunkname)
+               os.remove(tgz)
+          os.chdir(destpath)
+          shutil.rmtree(subd)
+     os.chdir(oldpath)
           
--- H2TauTau/python/harvest/test_harvest.py
+++ H2TauTau/python/harvest/test_harvest.py
@@ -6,10 +6,10 @@ import os
 import subprocess
 import time 
 
-from harvest import Harvester
+import harvest
 from datasetdb import DatasetDB 
 
-dsdb = DatasetDB(mode='writer')
+harvest.datasetdb = DatasetDB(mode='writer')
 infos = [
     { "tgz_pattern" : "*", "name" : "190503%DY1JetsToLL_M50_LO%tt_DY_nominal", "sub_date" : "2019-05-05_13-02-17", "tgzs" : { "0000" : [ "heppyOutput_1.tgz", "heppyOutput_10.tgz", "heppyOutput_11.tgz", "heppyOutput_12.tgz", "heppyOutput_13.tgz", "heppyOutput_14.tgz", "heppyOutput_15.tgz", "heppyOutput_16.tgz", "heppyOutput_17.tgz", "heppyOutput_18.tgz", "heppyOutput_19.tgz", "heppyOutput_2.tgz", "heppyOutput_20.tgz", "heppyOutput_21.tgz", "heppyOutput_22.tgz", "heppyOutput_23.tgz", "heppyOutput_24.tgz", "heppyOutput_25.tgz", "heppyOutput_26.tgz", "heppyOutput_27.tgz", "heppyOutput_28.tgz", "heppyOutput_29.tgz", "heppyOutput_3.tgz", "heppyOutput_30.tgz", "heppyOutput_31.tgz", "heppyOutput_32.tgz", "heppyOutput_33.tgz", "heppyOutput_34.tgz", "heppyOutput_35.tgz", "heppyOutput_36.tgz", "heppyOutput_37.tgz", "heppyOutput_38.tgz", "heppyOutput_39.tgz", "heppyOutput_4.tgz", "heppyOutput_40.tgz", "heppyOutput_41.tgz", "heppyOutput_5.tgz", "heppyOutput_6.tgz", "heppyOutput_7.tgz", "heppyOutput_8.tgz", "heppyOutput_9.tgz" ] }, "subdirs" : [ "0000" ], "prod_date" : "190503", "sample" : "DY1JetsToLL_M50_LO", "sub_dir" : "/gridgroup/cms/touquet/crab_submission_dirs/crab_DY1JetsToLL_M50_LO_tt_DY_nominal/crab_DY1JetsToLL_M50_LO_190503_tt_DY_nominal_2019-05-05_13-02-17", "subdir_pattern" : "*", "write_date" : "190505_110450", "path" : "/store/user/gtouquet/heppyTrees/190503/tt_DY_nominal/DY1JetsToLL_M50_LO/190505_110450", "sample_version" : "tt_DY_nominal" },
     { "tgz_pattern" : "*", "name" : "190503%DY1JetsToLL_M50_LO_ext%tt_DY_nominal", "sub_date" : "2019-05-05_13-04-54", "tgzs" : { "0000" : [ "heppyOutput_1.tgz", "heppyOutput_10.tgz", "heppyOutput_11.tgz", "heppyOutput_12.tgz", "heppyOutput_13.tgz", "heppyOutput_14.tgz", "heppyOutput_15.tgz", "heppyOutput_16.tgz", "heppyOutput_17.tgz", "heppyOutput_18.tgz", "heppyOutput_19.tgz", "heppyOutput_2.tgz", "heppyOutput_20.tgz", "heppyOutput_21.tgz", "heppyOutput_22.tgz", "heppyOutput_23.tgz", "heppyOutput_24.tgz", "heppyOutput_25.tgz", "heppyOutput_26.tgz", "heppyOutput_27.tgz", "heppyOutput_28.tgz", "heppyOutput_29.tgz", "heppyOutput_3.tgz", "heppyOutput_30.tgz", "heppyOutput_31.tgz", "heppyOutput_32.tgz", "heppyOutput_33.tgz", "heppyOutput_4.tgz", "heppyOutput_5.tgz", "heppyOutput_6.tgz", "heppyOutput_7.tgz", "heppyOutput_8.tgz", "heppyOutput_9.tgz" ] }, "subdirs" : [ "0000" ], "prod_date" : "190503", "sample" : "DY1JetsToLL_M50_LO_ext", "sub_dir" : "/gridgroup/cms/touquet/crab_submission_dirs/crab_DY1JetsToLL_M50_LO_ext_tt_DY_nominal/crab_DY1JetsToLL_M50_LO_ext_190503_tt_DY_nominal_2019-05-05_13-04-54", "subdir_pattern" : "*", "write_date" : "190505_110725", "path" : "/store/user/gtouquet/heppyTrees/190503/tt_DY_nominal/DY1JetsToLL_M50_LO_ext/190505_110725", "sample_version" : "tt_DY_nominal" },
@@ -19,42 +19,39 @@ class TestHarvest(unittest.TestCase):
 
     def setUp(self): 
         for info in infos:
-            dsdb.insert('se', info)
+            harvest.datasetdb.insert('se', info)
 
     def tearDown(self): 
         for info in infos: 
-            dsdb.remove('se', {'name':info['name']})
+            harvest.datasetdb.remove('se', {'name':info['name']})
 
     def test_1_ds_info(self):
         '''test that dataset info can be readout with a regex'''
-        harv = Harvester(dsdb)
-        infos = harv.get_ds_infos('se', '.*DY1Jets.*')
+        infos = harvest.get_ds_infos('se', '.*DY1Jets.*')
         self.assertEqual(len(infos), 2)
         for info in infos: 
             self.assertTrue(len(info['tgzs']['0000'])>1)
                              
     def test_2_fetch(self): 
-        harv = Harvester(dsdb)
-        infos = harv.get_ds_infos('se', '.*DY1Jets.*_ext')
+        infos = harvest.get_ds_infos('se', '.*DY1Jets.*_ext')
         outdir = tempfile.mkdtemp()
         # print(outdir)
         ntgzs = 2
-        harv.fetch(infos[0], outdir, ntgzs)
-        harv.unpack(infos[0], outdir, ntgzs)
+        harvest.fetch(infos[0], outdir, ntgzs)
+        harvest.unpack(infos[0], outdir, ntgzs)
         chunks = os.listdir(outdir)
         # pprint.pprint(chunks)
         self.assertListEqual(chunks, 
                              ['190503%DY1JetsToLL_M50_LO_ext%tt_DY_nominal_Chunk10',
                               '190503%DY1JetsToLL_M50_LO_ext%tt_DY_nominal_Chunk1']
                              )
-        harv.hadd(outdir)
+        harvest.hadd(outdir)
         results = os.listdir(outdir)
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0], '190503%DY1JetsToLL_M50_LO_ext%tt_DY_nominal')
         shutil.rmtree(outdir)
 
     def test_3_scp(self): 
-        harv = Harvester(dsdb)
         srcdir = tempfile.mkdtemp()
         dummy_fname = 'foo'
         open('/'.join([srcdir, dummy_fname]),'w').close()
@@ -63,13 +60,13 @@ class TestHarvest(unittest.TestCase):
         destdir = tempfile.mkdtemp()
         # print(srcdir)
         # print(destdir)
-        harv.scp(srcdir, 'localhost:'+destdir)
+        harvest.scp(srcdir, 'localhost:'+destdir)
         self.assertTrue(os.path.isfile('/'.join([destdir, 
                                                  os.path.basename(srcdir), 
                                                  dummy_fname])))
         
         # test copy to lyovis10 
-        harv.scp(srcdir, 'localhost:'+destdir, '-P 2222')
+        harvest.scp(srcdir, 'localhost:'+destdir, '-P 2222')
         result = subprocess.check_output(
             'ssh -p 2222 localhost ls {}'.format(destdir).split()
             )
@@ -80,21 +77,27 @@ class TestHarvest(unittest.TestCase):
         shutil.rmtree(destdir)
 
     def test_4_harvest_one(self):
-        harv = Harvester(dsdb)
-        infos = harv.get_ds_infos('se', '.*DY1Jets.*_ext')
-        destdir = '/data2/htt/unittests'
+        infos = harvest.get_ds_infos('se', '.*DY1Jets.*_ext')
+        destdir = '/data2/htt/unittests/single'
         info = infos[0]
         start = time.time()
-        harv.harvest_one(info, destdir, ntgzs=2)
+        hinfo = harvest.harvest_one(info, destdir, ntgzs=2)
+        harvest.datasetdb.insert('harvested', info)
         # check that dataset exists on destination: 
         result = subprocess.check_output(
             'ssh -p 2222 localhost ls {}'.format(destdir).split()
             )
         self.assertEqual(result.strip(), info['name'])
         # check harvesting time in db
-        hinfo = harv.get_ds_infos('harvested', info['name'])
+        hinfo = harvest.get_ds_infos('harvested', info['name'])
         self.assertEqual(len(hinfo),1)
         self.assertTrue(hinfo[0]['harv_time']>start)
+
+    def test_5_harvest_multi(self):
+        infos = harvest.get_ds_infos('se', '.*')
+        destdir = '/data2/htt/unittests/multi'
+        print(len(infos))
+        harvest.harvest(infos, destdir, ntgzs=2, nworkers=2)
 
         
 if __name__ == '__main__':
