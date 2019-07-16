d5b880af5afb11f46dd0f1b306da9de0f8b32f64
lucastorterotot <lucas.torterotot@gmail.com>
2019-07-16 14:51:35 +0200

    Extends voms-proxy-init validity to 192h (maximum)

--- H2TauTau/init.sh
+++ H2TauTau/init.sh
@@ -3,7 +3,7 @@ source /cvmfs/cms.cern.ch/cmsset_default.sh
 echo 'set crab3 environment'
 source /cvmfs/cms.cern.ch/crab3/crab.sh
 echo 'set cms vo proxy'
-voms-proxy-init --rfc --voms cms
+voms-proxy-init --rfc --voms cms --valid 192:00
 cd ../..
 echo 'set cmssw environment...'
 cmsenv
