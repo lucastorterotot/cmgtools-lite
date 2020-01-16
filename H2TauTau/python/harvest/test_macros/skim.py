02da6d4b5e6c6457a281308bb68b4db379e19f1e
lucastorterotot <lucas.torterotot@gmail.com>
2020-01-16 23:34:28 +0100

    Add veto_dilepton to mt and et channels skimming functions

--- H2TauTau/python/harvest/test_macros/skim.py
+++ H2TauTau/python/harvest/test_macros/skim.py
@@ -11,8 +11,8 @@ channel = 'tt'
 
 cuts = {
     'tt' : "Flag_goodVertices && Flag_globalTightHalo2016Filter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_BadPFMuonFilter && Flag_BadChargedCandidateFilter && Flag_ecalBadCalibFilter && (!veto_extra_elec) && (!veto_extra_muon) && l2_againstElectronVLooseMVA6 && l2_againstMuonLoose3 && l2_byVLooseIsolationMVArun2017v2DBoldDMwLT2017 && l1_byVLooseIsolationMVArun2017v2DBoldDMwLT2017 && l1_againstElectronVLooseMVA6 && l1_againstMuonLoose3",
-    'mt' : "Flag_goodVertices && Flag_globalTightHalo2016Filter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_BadPFMuonFilter && Flag_BadChargedCandidateFilter && Flag_ecalBadCalibFilter && (!veto_extra_elec) && (!veto_extra_muon) && l2_againstElectronVLooseMVA6 && l2_againstMuonLoose3 && l2_byVLooseIsolationMVArun2017v2DBoldDMwLT2017",
-    'et' : "Flag_goodVertices && Flag_globalTightHalo2016Filter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_BadPFMuonFilter && Flag_BadChargedCandidateFilter && Flag_ecalBadCalibFilter && (!veto_extra_elec) && (!veto_extra_muon) && l2_againstElectronVLooseMVA6 && l2_againstMuonLoose3 && l2_byVLooseIsolationMVArun2017v2DBoldDMwLT2017"
+    'mt' : "Flag_goodVertices && Flag_globalTightHalo2016Filter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_BadPFMuonFilter && Flag_BadChargedCandidateFilter && Flag_ecalBadCalibFilter && (!veto_extra_elec) && (!veto_extra_muon) && (!veto_dilepton) && l2_againstElectronVLooseMVA6 && l2_againstMuonLoose3 && l2_byVLooseIsolationMVArun2017v2DBoldDMwLT2017",
+    'et' : "Flag_goodVertices && Flag_globalTightHalo2016Filter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_BadPFMuonFilter && Flag_BadChargedCandidateFilter && Flag_ecalBadCalibFilter && (!veto_extra_elec) && (!veto_extra_muon) && (!veto_dilepton) && l2_againstElectronVLooseMVA6 && l2_againstMuonLoose3 && l2_byVLooseIsolationMVArun2017v2DBoldDMwLT2017"
 }
 
 meta = {
