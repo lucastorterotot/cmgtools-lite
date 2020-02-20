2df43555b77ec5385fd0a333902ef74c86ddc03c
sandhya jain <sandhya.jain@cern.ch>
2020-02-20 13:42:43 +0100

    added summer16 trigger and samples files

---
+++ TTbarTime/python/proto/samples/summer16/trigger.py
@@ -0,0 +1,162 @@
+from CMGTools.H2TauTau.proto.analyzers.TriggerAnalyzer import TriggerFilterMatch
+
+############################################################################
+# 2017
+############################################################################
+
+mc_triggers = [
+    'HLT_IsoMu24_v*',
+    'HLT_IsoMu22_eta2p1_v*',
+    'HLT_Ele27_WPTight Gsf_v*',
+    'HLT_Ele32_eta2p1_WPTight_Gsf_v*',
+    'HLT_Ele25_eta2p1_WPTight_Gsf_v*',
+    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*',
+    'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*',
+    'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+    'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v*',
+    'HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v*'
+    'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL DZ_v*',
+    'HLT_DoubleEle24_22_eta2p1_WPLoose_Gsf_v*',
+]
+
+
+#data_triggers = {}
+#
+#data_triggers['A'] = [
+#    # electron
+#    'HLT_Ele32_WPTight_Gsf_L1DoubleEG_v*',
+#    'HLT_Ele35_WPTight_Gsf_v*',
+#    'HLT_Ele38_WPTight_Gsf_v*',
+#    'HLT_Ele40_WPTight_Gsf_v*',
+#    # double electron
+#    'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+#    # muon
+#    'HLT_IsoTkMu24_v*',
+#    'HLT_IsoMu24_v*',
+#    'HLT_IsoMu24_eta2p1_v*',
+#    'HLT_IsoMu27_v*',
+#    # double muon
+#    'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v*',
+#    # electron-muon
+#    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+#    'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*',
+#    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*'
+#]
+#data_triggers['B'] = data_triggers['A']
+#
+#data_triggers['C'] = [
+#    # electron
+#    'HLT_Ele32_WPTight_Gsf_L1DoubleEG_v*',
+#    'HLT_Ele35_WPTight_Gsf_v*',
+#    'HLT_Ele38_WPTight_Gsf_v*',
+#    'HLT_Ele40_WPTight_Gsf_v*',
+#    # double electron
+#    'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+#    # muon
+#    'HLT_IsoMu24_eta2p1_v*',
+#    'HLT_IsoMu27_v*',
+#    # double muon
+#    'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v*',
+#    'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v*',
+#    # electron-muon
+#    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+#    'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*',
+#    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*'
+#]
+#
+#data_triggers['D'] = [
+#    # electron
+#    'HLT_Ele32_WPTight_Gsf_v*',
+#    'HLT_Ele35_WPTight_Gsf_v*',
+#    'HLT_Ele38_WPTight_Gsf_v*',
+#    'HLT_Ele40_WPTight_Gsf_v*',
+#    # double electron
+#    'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+#    # muon
+#    'HLT_IsoMu24_eta2p1_v*',
+#    'HLT_IsoMu27_v*',
+#    # double muon
+#    'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v*',
+#    'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v*',
+#    # electron-muon
+#    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+#    'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*',
+#    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*'
+#]
+#
+#data_triggers['E'] = [
+#    # electron
+#    'HLT_Ele32_WPTight_Gsf_v*',
+#    'HLT_Ele35_WPTight_Gsf_v*',
+#    'HLT_Ele38_WPTight_Gsf_v*',
+#    'HLT_Ele40_WPTight_Gsf_v*',
+#    # double electron
+#    'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+#    # muon
+#    'HLT_IsoMu27_v*',
+#    # double muon
+#    'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v*',
+#    'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v*',
+#    # electron-muon
+#    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+#    'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*',
+#    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*'
+#]
+#
+#data_triggers['F'] = [
+#    # electron
+#    'HLT_Ele32_WPTight_Gsf_v*',
+#    'HLT_Ele35_WPTight_Gsf_v*',
+#    'HLT_Ele38_WPTight_Gsf_v*',
+#    'HLT_Ele40_WPTight_Gsf_v*',
+#    # double electron
+#    'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+#    # muon
+#    'HLT_IsoMu27_v*',
+#    # double muon
+#    'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v*',
+#    'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v*',
+#    # electron-muon
+#    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*',
+#    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v*',
+#    'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*',
+#    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*'
+#]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TTbarTime/python/proto/samples/summer16/ttbar2016.py
@@ -0,0 +1,117 @@
+import os 
+from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
+
+creator = ComponentCreator()
+
+json = os.path.expandvars('$CMSSW_BASE/src/CMGTools/TTbarTime/data/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt')
+lumi = 41529.
+
+############################################################################
+# MC
+############################################################################
+
+signal_MC_dilep = creator.makeMCComponent("MC_a_dilep","/TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v1/MINIAODSIM","CMS",".*root", 88.2);
+signal_MC_semilep = creator.makeMCComponent("MC_b_semilep","/TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM","CMS",".*root", 365.3);
+
+background_MC_TTW = creator.makeMCComponent("MC_c_TTW","/TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM", "CMS", ".*root",0.2043);
+background_MC_TTZ = creator.makeMCComponent("MC_d_TTZ","/TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM", "CMS", ".*root",0.2529);
+
+background_MC_ST_s = creator.makeMCComponent("MC_e_ST_s","/ST_s-channel_4f_leptonDecays_TuneCP5_PSweights_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM", "CMS", ".*root",3.36);
+background_MC_ST_t_top = creator.makeMCComponent("MC_f_ST_t_top","/ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM","CMS",".*root",136.02);
+background_MC_ST_t_antitop = creator.makeMCComponent("MC_g_ST_t_antitop","/ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM","CMS",".*root",80.95);
+background_MC_tW_top = creator.makeMCComponent("MC_h_ST_tW_top","/ST_tW_top_5f_inclusiveDecays_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM", "CMS", ".*root",35.9);
+background_MC_tW_antitop = creator.makeMCComponent("MC_i_ST_tW_antitop","/ST_tW_antitop_5f_inclusiveDecays_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM", "CMS", ".*root",35.9);
+
+background_MC_WW = creator.makeMCComponent("MC_j_WW", "/WW_TuneCP5_13TeV-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM", "CMS", ".*root", 118.7)
+background_MC_WZ = creator.makeMCComponent("MC_k_WZ", "/WZ_TuneCP5_13TeV-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM", "CMS", ".*root", 47.13)
+background_MC_ZZ = creator.makeMCComponent("MC_l_ZZ", "/ZZ_TuneCP5_13TeV-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v2/MINIAODSIM", "CMS", ".*root", 16.523)
+background_MC_WJets = creator.makeMCComponent("MC_m_WJets", "/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v3/MINIAODSIM", "CMS", ".*root", 61526.7)
+
+background_MC_DY_50 = creator.makeMCComponent("MC_n_DY_50", "/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM", "CMS", ".*root", 6025.2)
+background_MC_DY_1050 = creator.makeMCComponent("MC_o_DY_1050", "/DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v2/MINIAODSIM", "CMS", ".*root", 22635.1)
+
+
+mc_ttbar = [
+    signal_MC_dilep,
+    #signal_MC_semilep,
+    #background_MC_TTW,
+    #background_MC_TTZ,
+    #background_MC_ST_s,
+    #background_MC_ST_t_top,
+    #background_MC_ST_t_antitop,
+    #background_MC_tW_top,
+    #background_MC_tW_antitop,
+    #background_MC_WW, 
+    #background_MC_WZ,
+    #background_MC_ZZ,
+    #background_MC_WJets,
+    #background_MC_DY_50,
+    #background_MC_DY_1050
+]
+
+############################################################################
+# MC
+############################################################################
+
+# Run2017B 31Mar2018
+
+SingleElectron_Run2017B_31Mar2018 = creator.makeDataComponent("SingleElectron_Run2017B_31Mar2018", "/SingleElectron/Run2017B-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+SingleMuon_Run2017B_31Mar2018 = creator.makeDataComponent("SingleMuon_Run2017B_31Mar2018", "/SingleMuon/Run2017B-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+
+DoubleEG_Run2017B_31Mar2018 = creator.makeDataComponent("DoubleEG_Run2017B_31Mar2018", "/DoubleEG/Run2017B-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+MuonEG_Run2017B_31Mar2018 = creator.makeDataComponent("MuonEG_Run2017B_31Mar2018", "/MuonEG/Run2017B-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+DoubleMuon_Run2017B_31Mar2018 = creator.makeDataComponent("DoubleMuon_Run2017B_31Mar2018", "/DoubleMuon/Run2017B-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+
+# Run2017C 31Mar2018
+
+SingleElectron_Run2017C_31Mar2018 = creator.makeDataComponent("SingleElectron_Run2017C_31Mar2018", "/SingleElectron/Run2017C-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+SingleMuon_Run2017C_31Mar2018 = creator.makeDataComponent("SingleMuon_Run2017C_31Mar2018", "/SingleMuon/Run2017C-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+
+DoubleEG_Run2017C_31Mar2018 = creator.makeDataComponent("DoubleEG_Run2017C_31Mar2018", "/DoubleEG/Run2017C-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+MuonEG_Run2017C_31Mar2018 = creator.makeDataComponent("MuonEG_Run2017C_31Mar2018", "/MuonEG/Run2017C-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+DoubleMuon_Run2017C_31Mar2018 = creator.makeDataComponent("DoubleMuon_Run2017C_31Mar2018", "/DoubleMuon/Run2017C-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+
+# Run2017D 31Mar2018
+
+SingleElectron_Run2017D_31Mar2018 = creator.makeDataComponent("SingleElectron_Run2017D_31Mar2018", "/SingleElectron/Run2017D-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+SingleMuon_Run2017D_31Mar2018 = creator.makeDataComponent("SingleMuon_Run2017D_31Mar2018", "/SingleMuon/Run2017D-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+
+DoubleEG_Run2017D_31Mar2018 = creator.makeDataComponent("DoubleEG_Run2017D_31Mar2018", "/DoubleEG/Run2017D-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+MuonEG_Run2017D_31Mar2018 = creator.makeDataComponent("MuonEG_Run2017D_31Mar2018", "/MuonEG/Run2017D-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+DoubleMuon_Run2017D_31Mar2018 = creator.makeDataComponent("DoubleMuon_Run2017D_31Mar2018", "/DoubleMuon/Run2017D-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+
+# Run2017E 31Mar2018
+
+SingleElectron_Run2017E_31Mar2018 = creator.makeDataComponent("SingleElectron_Run2017E_31Mar2018", "/SingleElectron/Run2017E-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+SingleMuon_Run2017E_31Mar2018 = creator.makeDataComponent("SingleMuon_Run2017E_31Mar2018", "/SingleMuon/Run2017E-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+
+DoubleEG_Run2017E_31Mar2018 = creator.makeDataComponent("DoubleEG_Run2017E_31Mar2018", "/DoubleEG/Run2017E-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+MuonEG_Run2017E_31Mar2018 = creator.makeDataComponent("MuonEG_Run2017E_31Mar2018", "/MuonEG/Run2017E-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+DoubleMuon_Run2017E_31Mar2018 = creator.makeDataComponent("DoubleMuon_Run2017E_31Mar2018", "/DoubleMuon/Run2017E-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+
+# Run2017F 31Mar2018
+
+SingleElectron_Run2017F_31Mar2018 = creator.makeDataComponent("SingleElectron_Run2017F_31Mar2018", "/SingleElectron/Run2017F-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+SingleMuon_Run2017F_31Mar2018 = creator.makeDataComponent("SingleMuon_Run2017F_31Mar2018", "/SingleMuon/Run2017F-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+
+DoubleEG_Run2017F_31Mar2018 = creator.makeDataComponent("DoubleEG_Run2017F_31Mar2018", "/DoubleEG/Run2017F-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+MuonEG_Run2017F_31Mar2018 = creator.makeDataComponent("MuonEG_Run2017F_31Mar2018", "/MuonEG/Run2017F-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+DoubleMuon_Run2017F_31Mar2018 = creator.makeDataComponent("DoubleMuon_Run2017F_31Mar2018", "/DoubleMuon/Run2017F-31Mar2018-v1/MINIAOD", "CMS", ".*root", json)
+
+
+# les lists 
+
+data_single_electron = [SingleElectron_Run2017B_31Mar2018, SingleElectron_Run2017C_31Mar2018, SingleElectron_Run2017D_31Mar2018, SingleElectron_Run2017E_31Mar2018, SingleElectron_Run2017F_31Mar2018]
+
+data_single_muon = [SingleMuon_Run2017B_31Mar2018, SingleMuon_Run2017C_31Mar2018, SingleMuon_Run2017D_31Mar2018, SingleMuon_Run2017E_31Mar2018, SingleMuon_Run2017F_31Mar2018]
+
+data_muon_electron = [MuonEG_Run2017B_31Mar2018, MuonEG_Run2017C_31Mar2018, MuonEG_Run2017D_31Mar2018, MuonEG_Run2017E_31Mar2018, MuonEG_Run2017F_31Mar2018]
+
+data_dimuon = [DoubleMuon_Run2017B_31Mar2018, DoubleMuon_Run2017C_31Mar2018, DoubleMuon_Run2017D_31Mar2018, DoubleMuon_Run2017E_31Mar2018, DoubleMuon_Run2017F_31Mar2018]
+
+data_dielectron = [DoubleEG_Run2017B_31Mar2018, DoubleEG_Run2017C_31Mar2018, DoubleEG_Run2017D_31Mar2018, DoubleEG_Run2017E_31Mar2018, DoubleEG_Run2017F_31Mar2018]
+
+data_elecmuon = data_single_electron + data_single_muon + data_muon_electron
+data_ttbar = data_elecmuon + data_dimuon + data_dielectron
+
+
